df1a4dd5131ec4fb93fc0d8bf3e4a815451bbad8
RK Sharma <rksiitd123@gmail.com>
2024-08-15 01:36:10 +0530

    Configure next js

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+// next.config.js
+module.exports = {
+    reactStrictMode: true,
+    swcMinify: true,
+    // Ensure that Next.js outputs a static website
+    output: 'export',
+    // Disable image optimization (not supported on Cloudflare Pages)
+    images: {
+      unoptimized: true,
+    },
+  }
